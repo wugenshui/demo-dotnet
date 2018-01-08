bb346a68747b1450bf56bbe321cb19a9cef2e6b0
1 <1>
2018-01-08 14:20:38 +0800

    rsa算法

--- register/Properties/AssemblyInfo.cs
+++ register/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Register")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("0cee296e-ed08-4cb3-95fa-21cfa949cdf5")]
+[assembly: Guid("dd13571e-bc89-48e4-a841-a00688afd78a")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
