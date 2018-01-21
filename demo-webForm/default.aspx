a0d2d7143e9996877ff7353bd496eeef6e84b52c
r720 <963838470@qq.com>
2018-01-21 21:34:10 +0800

    web记录日志

--- demo-webForm/default.aspx
+++ demo-webForm/default.aspx
@@ -4,14 +4,14 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title></title>
 </head>
 <body>
     <form id="form1" runat="server">
-    <div>
-    123
-    </div>
+        <div>
+            <asp:Button runat="server" OnClick="Unnamed_Click" Text="记录日志" />
+        </div>
     </form>
 </body>
 </html>
--- demo-webForm/default.aspx.cs
+++ demo-webForm/default.aspx.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -12,7 +13,15 @@ namespace demo_webForm
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            LogHelper.Debug("123");
+            if (!IsPostBack)
+            {
+                LogHelper.Info("日志初始化");
+            }
+        }
+
+        protected void Unnamed_Click(object sender, EventArgs e)
+        {
+            LogHelper.Debug("button click");
         }
     }
 }
