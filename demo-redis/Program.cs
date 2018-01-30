ec9a0e86738102f7f04f04e5c537c53f4fe0ff23
963838470 <963838470@qq.com>
2018-01-30 12:12:41 +0800

    redis

---
+++ demo-redis/Program.cs
@@ -0,0 +1,23 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo_redis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(ConfigurationManager.AppSettings["connect"]);
+            IDatabase db = redis.GetDatabase();
+            string value = "1";
+            //db.StringSet("mykey", value);
+            value = db.StringGet("mykey");
+            Console.WriteLine(value);
+        }
+    }
+}
