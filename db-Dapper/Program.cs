2ce5fbd9d186e9a811ea0938f1168fe4f95797ca
963838470 <963838470@qq.com>
2018-02-28 14:43:21 +0800

    SqlSugar

--- db-Dapper/Program.cs
+++ db-Dapper/Program.cs
@@ -9,14 +9,14 @@ using System.Threading.Tasks;
 
 namespace db_Dapper
 {
-    class ou_user
+    class OuUser
     {
         public int id { get; set; }
         public int ouid { get; set; }
         public int userid { get; set; }
     }
 
-    class ou
+    class Ou
     {
         public int id { get; set; }
         public int pid { get; set; }
@@ -29,10 +29,10 @@ namespace db_Dapper
         {
             IDbConnection connection = new SqlConnection("Data Source=.;Initial Catalog=OA;Integrated Security=True;MultipleActiveResultSets=True");
             string sql = "INSERT INTO AuthorityOuUser (ouId, userId) VALUES (@ouid, @userid)";
-            ou_user model = new ou_user { ouid = 86, userid = 31 };
+            OuUser model = new OuUser { ouid = 86, userid = 31 };
             //int result = connection.Execute(sql, model);    // 单行插入
 
-            List<ou_user> models = new List<ou_user>();
+            List<OuUser> models = new List<OuUser>();
             models.Add(model);
             models.Add(model);
             //int result2 = connection.Execute(sql, models);    // 多行插入
@@ -42,9 +42,9 @@ namespace db_Dapper
             int result3 = connection.Execute("UPDATE AuthorityOuUser SET ouId=10086 WHERE id=@id", new { id = 11 }); // 修改
 
             // 查询
-            List<ou_user> data = connection.Query<ou_user>("SELECT id, ouId, userId FROM AuthorityOuUser").ToList();
-            List<ou_user> data2 = connection.Query<ou_user>("SELECT id, ouId, userId FROM AuthorityOuUser WHERE ouId=@ouId", new { ouid = 86 }).ToList();
-            List<ou_user> data3 = connection.Query<ou_user>("SELECT id, ouId, userId FROM AuthorityOuUser WHERE ouId in @ouId", new { ouid = new int[2] { 86, 1 } }).ToList();
+            List<OuUser> data = connection.Query<OuUser>("SELECT id, ouId, userId FROM AuthorityOuUser").ToList();
+            List<OuUser> data2 = connection.Query<OuUser>("SELECT id, ouId, userId FROM AuthorityOuUser WHERE ouId=@ouId", new { ouid = 86 }).ToList();
+            List<OuUser> data3 = connection.Query<OuUser>("SELECT id, ouId, userId FROM AuthorityOuUser WHERE ouId in @ouId", new { ouid = new int[2] { 86, 1 } }).ToList();
             var data4 = connection.Query("SELECT * FROM AuthorityOu o LEFT JOIN AuthorityOuUser u ON u.ouId=o.id").ToList();
             foreach (var item in data4)
             {
---
+++ db-SqlSugar/Program.cs
@@ -0,0 +1,57 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace db_SqlSugar
+{
+    public class OuUser
+    {
+        public int id { get; set; }
+        public int ouid { get; set; }
+        public int userid { get; set; }
+    }
+
+    [SugarTable("AuthorityOu")]
+    public class AuthorityOu
+    {
+        [SugarColumn(IsPrimaryKey = true, IsIdentity = true)]
+        public int id { get; set; }
+        public int pid { get; set; }
+        public string name { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //创建SqlSugarClient对象
+            var db = GetInstance();
+            var sdb = new SimpleClient<AuthorityOu>(db);
+
+            var sss = db.SqlQueryable<AuthorityOu>("select * from AuthorityOu").ToPageList(1, 2);
+
+            var _AuthorityOu = db.GetSimpleClient<AuthorityOu>();
+            var firstOu = _AuthorityOu.GetById(1);
+            var booll = _AuthorityOu.Insert(new AuthorityOu()
+             {
+                name = "1",
+                pid = 1,
+                id = 1
+            });
+        }
+
+        public static SqlSugarClient GetInstance()
+        {
+            SqlSugarClient db = new SqlSugarClient(new ConnectionConfig() { ConnectionString = "server=.;uid=sa;pwd=sa;database=OA", DbType = DbType.SqlServer, IsAutoCloseConnection = true });
+            db.Aop.OnLogExecuting = (sql, pars) =>
+            {
+                Console.WriteLine(sql + "\r\n" + db.Utilities.SerializeObject(pars.ToDictionary(it => it.ParameterName, it => it.Value)));
+                Console.WriteLine();
+            };
+            return db;
+        }
+    }
+}
