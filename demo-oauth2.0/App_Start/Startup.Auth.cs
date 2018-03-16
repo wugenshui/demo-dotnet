6d1b243972f202fb3d3a6892de4c82e2ec4963d7
963838470 <963838470@qq.com>
2018-03-16 11:27:20 +0800

    Startup 合并

--- demo-oauth2.0/App_Start/Startup.Auth.cs
+++ demo-oauth2.0/App_Start/Startup.Auth.cs
@@ -1,9 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using demoOAuth2.Providers;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
 using Microsoft.Owin.Security;
-using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.Infrastructure;
 using Microsoft.Owin.Security.OAuth;
-using demoOAuth2.Providers;
 using Owin;
 using System;
 using System.Collections.Concurrent;
@@ -11,11 +10,23 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 
 namespace demoOAuth2
 {
     public partial class Startup
     {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            ConfigureAuth(app);
+
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseCors(CorsOptions.AllowAll);  // 允许跨域请求
+            app.UseWebApi(config);
+        }
+
         public void ConfigureAuth(IAppBuilder app)
         {
             var OAuthOptions = new OAuthAuthorizationServerOptions
