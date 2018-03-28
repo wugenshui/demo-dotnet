ef8e95a7401245da55478564ebcb2e1ff7477a4f
963838470 <963838470@qq.com>
2018-03-28 15:19:45 +0800

    ef xml文件读取

---
+++ EFAddNote/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace EFAddNote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dirpath = Environment.CurrentDirectory;
+            DirectoryInfo dir = new DirectoryInfo(dirpath);
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                if (file.Name.Contains(".edmx"))
+                {
+                    Console.WriteLine(file.Name);
+                    XmlDocument doc = new XmlDocument();
+                    doc.Load(file.FullName);
+                    XmlElement root = doc.DocumentElement;
+                    XmlNode runtime = root.GetElementsByTagName("edmx:Runtime")[0];
+                    XmlNode models = root.GetElementsByTagName("edmx:ConceptualModels")[0];
+                    XmlNode schema = root.GetElementsByTagName("Schema")[0];
+                    XmlNodeList entitys = root.GetElementsByTagName("EntityType");
+                    foreach (XmlNode entity in entitys)
+                    {
+                        string tablename = entity.Attributes["Name"].Value;
+                        XmlNodeList props = root.GetElementsByTagName("Property");
+                        foreach (XmlNode prop in props)
+                        {
+                            string rowname = prop.Attributes["Name"].Value;
+                            Console.WriteLine(tablename + ":" + rowname);
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
