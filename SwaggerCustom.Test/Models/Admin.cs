a67b326e078bc0f540dc16f2c68ca6dabf534f46
963838470 <963838470@qq.com>
2018-04-25 16:14:35 +0800

    swagger demo

---
+++ SwaggerCustom.Test/Models/Admin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SwaggerCustom.Test.Models
+{
+    /// <summary>
+    /// 管理员
+    /// </summary>
+    public class Admin
+    {
+        /// <summary>
+        /// 管理员姓名
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
