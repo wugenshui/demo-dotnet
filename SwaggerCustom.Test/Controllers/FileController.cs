317252eeee22dfde4243d5e5d2a32b5ad79185c3
wugenshui <963838470@qq.com>
2018-07-26 11:17:39 +0800

    文件上传测试 失败

---
+++ SwaggerCustom.Test/Controllers/FileController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace SwaggerCustom.Test.Controllers
+{
+    public class FileController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Post([FromBody]FileInfo file)
+        {
+            return Json(file.FullName);
+        }
+    }
+}
