88e840ae362b94a249369279e8d67aa130441921
r720 <963838470@qq.com>
2018-10-15 23:12:47 +0800

    工厂方法

---
+++ DesignPattern/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FactoryMethod();
+
+            Console.ReadKey();
+        }
+
+        #region 创建型模式
+
+        // 工厂方法
+        static void FactoryMethod()
+        {
+            Operation operate = OperationFactory.createOperate("+");
+            operate.NumberA = 1;
+            operate.NumberB = 2;
+            Console.WriteLine("1+2=" + operate.GetResult());
+
+            operate = OperationFactory.createOperate("-");
+            operate.NumberA = 1;
+            operate.NumberB = 2;
+            Console.WriteLine("1-2=" + operate.GetResult());
+
+            operate = OperationFactory.createOperate("*");
+            operate.NumberA = 1;
+            operate.NumberB = 2;
+            Console.WriteLine("1*2=" + operate.GetResult());
+
+            operate = OperationFactory.createOperate("/");
+            operate.NumberA = 1;
+            operate.NumberB = 2;
+            Console.WriteLine("1/2=" + operate.GetResult());
+        }
+
+        #endregion
+    }
+}
---
+++ DesignPattern/工厂方法/OperaterDiv.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    public class OperationDiv : Operation
+    {
+        public override double GetResult()
+        {
+            if (NumberB == 0)
+                throw new Exception("除数不能为0！");
+            return NumberA / NumberB;
+        }
+    }
+}
