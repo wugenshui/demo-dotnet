823e80a6b74aee112c0d508d9f2e5fa76570069d
wugenshui <963838470@qq.com>
2018-10-25 11:58:22 +0800

    语音播报

--- demo-winform/Form1.cs
+++ demo-winform/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DotNetSpeech;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,9 +12,31 @@ namespace demo_winform
 {
     public partial class Form1 : Form
     {
+        public string msg = "黄伟均身份已识别，欢迎登录系统！";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            VoiceHelper.PlayVoice(msg);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SpVoice m_voice = VoiceHelper.m_voice;
+            m_voice.Rate = -1;
+            SpeechVoiceSpeakFlags spFlags = SpeechVoiceSpeakFlags.SVSFDefault;
+            m_voice.Speak(msg, spFlags);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SpVoice m_voice = VoiceHelper.m_voice;
+            m_voice.Rate = -1;
+            m_voice.Speak(msg);
         }
     }
 }
---
+++ demo-winform/VoiceHelper.cs
@@ -0,0 +1,34 @@
+﻿using DotNetSpeech;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace demo_winform
+{
+    class VoiceHelper
+    {
+        public static SpVoice m_voice = new SpVoice();
+
+        /// <summary>
+        /// 根据文字播放出语音
+        /// </summary>
+        /// <param name="text">文字</param>
+        public static void PlayVoice(string text, Boolean closeVoice = true)
+        {
+            m_voice.Rate = -1;
+            SpeechVoiceSpeakFlags spFlags = SpeechVoiceSpeakFlags.SVSFlagsAsync;
+            m_voice.Speak(text, spFlags);
+            //m_voice.WaitUntilDone(System.Threading.Timeout.Infinite);
+            //Thread.Sleep(500);
+        }
+
+
+        public static void CloseVoice()
+        {
+            SpeechVoiceSpeakFlags spFlags = SpeechVoiceSpeakFlags.SVSFPurgeBeforeSpeak;
+            m_voice.Speak("", spFlags);
+        }
+    }
+}
