8b4f6e8628d8eabde67f340010c21beafc20b638
wugenshui <963838470@qq.com>
2018-11-26 12:10:46 +0800

    修改文件提交时间

--- demo-winform/Form1.cs
+++ demo-winform/Form1.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -21,7 +22,26 @@ namespace demo_winform
 
         private void button1_Click(object sender, EventArgs e)
         {
-            VoiceHelper.PlayVoice(msg);
+            //VoiceHelper.PlayVoice(msg);
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "请选择要打开的文件";
+            ofd.Multiselect = false;
+            //ofd.InitialDirectory = @"C:\Uss\shaofeng\Desktop";
+            ofd.Filter = "文本文件|*.txt|所有文件|*.*";
+            ofd.ShowDialog();
+            string path = ofd.FileName;
+            if (path == "")
+            {
+                return;
+            }
+
+            FileInfo file = new FileInfo(path);
+
+            DateTime date = DateTime.Today.AddDays(-1);
+            file.CreationTime = date;
+            file.LastWriteTime = date;
+            file.LastAccessTime = date;
         }
 
         private void button2_Click(object sender, EventArgs e)
