3e7556e5417817dd040f05c36121b4d353c1cafb
wugenshui <963838470@qq.com>
2018-12-05 14:46:27 +0800

    adb使用

---
+++ adb/CmdAdbInfo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace adb
+{
+    public class CmdAdbInfo
+    {
+        /* 指定设备 */
+        public static string adb_universal_s = "-s";
+        public static string adb_universal_p = "-P";
+        /**启动adb服务*/
+        public static string adb_start_server = "start-server";
+        /**停止adb服务*/
+        public static string adb_kill_server = "kill-server";
+        /**重启adb服务*/
+        public static string adb_reboot = "reboot";
+        /**获取adb的版本信息*/
+        public static string adb_version = "version";
+        /**获取连接的设备信息*/
+        public static string adb_devices = "devices";
+        /**列出手机装的所有app包名*/
+        public static string adb_shell_pm_list_packages = "shell pm list packages";
+        /**列出系统应用app包名*/
+        public static string adb_shell_pm_list_packages_s = "shell pm list packages s";
+        /**列出第三方应用app包名*/
+        public static string adb_shell_pm_list_packages_3 = "shell pm list packages -3";
+        /**安装软件*/
+        public static string adb_install = "install";
+        /**卸载软件*/
+        public static string adb_uninstall = "uninstall";
+        /**电池状况*/
+        public static string adb_shell_dumpsys_battery = "shell dumpsys battery";
+        /**屏幕分辨率*/
+        public static string adb_shell_wm_size = "shell wm size";
+        /**屏幕信息*/
+        public static string adb_shell_dumpsys_window_displays = "shell dumpsys window displays";
+        /**网络配置*/
+        public static string adb_shell_netcfg = "shell netcfg";
+        /**读取设备文件内容**/
+        public static string adb_shell_cat = "shell cat";
+        /**root*/
+        public static string adb_root = "root";
+        /**复制设备文件到电脑**/
+        public static string adb_pull = "pull";
+        /**复制电脑文件到设备**/
+        public static string adb_push = "push";
+
+        /**获取所有adb指令*/
+        public static List<string> GetAllADBCmdList()
+        {
+            List<string> itemsList = new List<string>();
+            itemsList.Add(adb_start_server);
+            itemsList.Add(adb_kill_server);
+            itemsList.Add(adb_reboot);
+            itemsList.Add(adb_version);
+            itemsList.Add(adb_devices);
+            itemsList.Add(adb_shell_pm_list_packages);
+            itemsList.Add(adb_shell_pm_list_packages_s);
+            itemsList.Add(adb_shell_pm_list_packages_3);
+            itemsList.Add(adb_install);
+            itemsList.Add(adb_uninstall);
+            itemsList.Add(adb_shell_dumpsys_battery);
+            itemsList.Add(adb_shell_wm_size);
+            itemsList.Add(adb_shell_dumpsys_window_displays);
+            itemsList.Add(adb_shell_netcfg);
+            itemsList.Add(adb_shell_cat);
+            itemsList.Add(adb_root);
+            itemsList.Add(adb_pull);
+            itemsList.Add(adb_push);
+            itemsList.Sort();
+            return itemsList;
+        }
+
+    }
+}
---
+++ adb/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace adb
+{
+    class Program
+    {
+        CmdHelp cmdHelp = new CmdHelp();
+        SynchronizationContext m_SyncContext = null;
+        public string m_ReviceMsgStr = string.Empty;
+
+        static void Main(string[] args)
+        {
+            var m_SyncContext = SynchronizationContext.Current;
+            new Program().StartADB();
+        }
+
+        void StartADB()
+        {
+            // 注册事件(adb,Android Debug Bridge)
+            CmdHelp.EventReceiveData += new CmdHelp.DelegateReceiveData(cmdHelp_EventReceiveData);
+            CmdHelp.EventReceiveThreadData += new CmdHelp.DelegateReceiveThreadData(CmdHelp_EventReceiveThreadData);
+            CmdHelp.EventReceiveThreadErrorData += new CmdHelp.DelegateReceiveThreadErrorData(CmdHelp_EventReceiveThreadErrorData);
+            m_SyncContext = SynchronizationContext.Current;
+
+            cmdHelp.StartServer("10010"); // 防止端口占用
+            string[] devices = cmdHelp.GetDevices();
+            foreach (var name in devices)
+            {
+                Console.WriteLine("设备：" + name);
+                string[] apps = cmdHelp.GetAPP(name);
+                foreach (var app in apps)
+                {
+                    Console.WriteLine("\t" + app);
+                }
+                string file = name + ".txt";
+                string remote = "/sdcard/" + file;
+                File.WriteAllText(file, string.Join(",", apps));
+                var pushResult = cmdHelp.FilePush(name, file, remote);
+                Console.WriteLine("上传结果：" + pushResult);
+                var pullResult = cmdHelp.FilePull(name, remote, "pull.txt");
+                Console.WriteLine("下载结果：" + pullResult);
+            }
+
+            Console.Read();
+        }
+
+        //接收到数据
+        private void cmdHelp_EventReceiveData(string data)
+        {
+            //m_SyncContext.Post(setListData, data);
+        }
+
+        //接收错误数据
+        private void CmdHelp_EventReceiveThreadErrorData(string data)
+        {
+            //m_SyncContext.Post(setListData, data);
+        }
+
+
+        void CmdHelp_EventReceiveThreadData(string data)
+        {
+            //m_SyncContext.Post(setListData, data);
+        }
+
+        private void setListData(object data)
+        {
+            if (data != null)
+            {
+                m_ReviceMsgStr = data.ToString();
+            }
+        }
+    }
+}
