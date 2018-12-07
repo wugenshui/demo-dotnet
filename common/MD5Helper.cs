f6a5e2275b6174e57ee3cf081efbf6aeff75aebd
wugenshui <963838470@qq.com>
2018-12-07 10:59:57 +0800

    MD5注释

--- common/MD5Helper.cs
+++ common/MD5Helper.cs
@@ -10,15 +10,15 @@ namespace common
     public class MD5Helper
     {
         /// <summary>  
-        /// 获取文件的MD5码  
+        /// 获取文件的MD5码
         /// </summary>  
-        /// <param name="fileName">传入的文件名（含路径及后缀名）</param>  
+        /// <param name="fileName">传入的文件名（含路径及后缀名）</param>
         /// <returns></returns>  
         public static string GetMD5FromFile(string fileName)
         {
             try
             {
-                FileStream file = new FileStream(fileName, System.IO.FileMode.Open);
+                FileStream file = new FileStream(fileName, FileMode.Open);
                 MD5 md5 = new MD5CryptoServiceProvider();
                 byte[] retVal = md5.ComputeHash(file);
                 file.Close();
@@ -35,11 +35,16 @@ namespace common
             }
         }
 
+        /// <summary>
+        /// 获取字符串的MD5码
+        /// </summary>
+        /// <param name="input">要处理的字符串</param>
+        /// <returns></returns>
         public static string CreateMD5(string input)
         {
             // Use input string to calculate MD5 hash
-            MD5 md5 = System.Security.Cryptography.MD5.Create();
-            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            MD5 md5 = MD5.Create();
+            byte[] inputBytes = Encoding.ASCII.GetBytes(input);
             byte[] hashBytes = md5.ComputeHash(inputBytes);
 
             // Convert the byte array to hexadecimal string
