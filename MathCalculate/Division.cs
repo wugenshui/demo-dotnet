005b2fa333d5273c443a44abfa04c00327c16647
wugenshui <963838470@qq.com>
2019-03-13 18:28:13 +0800

    mstest

---
+++ MathCalculate/Division.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MathCalculate
+{
+    /// <summary>
+    /// 除法类
+    /// </summary>
+    public static class Division
+    {
+        /// <summary>
+        /// 除法调用
+        /// </summary>
+        /// <param name="divisor">除数</param>
+        /// <param name="dividend">被除数</param>
+        /// <returns></returns>
+        public static double Calculation(double divisor, double dividend)
+        {
+            if (dividend == 0)
+            {
+                throw new Exception("除数不能为0");
+            }
+            return divisor / dividend;
+        }
+    }
+}
---
+++ MathCalculateMSTests/DivisionTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathCalculate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MathCalculate.MSTests
+{
+    [TestClass()]
+    public class DivisionTests
+    {
+        [TestMethod()]
+        public void CalculationTest_Return5()
+        {
+            Assert.AreEqual(5, Division.Calculation(5, 1));
+
+            Assert.AreEqual(2.5, Division.Calculation(5, 2));
+
+            Assert.AreEqual(2, Division.Calculation(4, 2));
+
+            Assert.AreEqual(5, Division.Calculation(5, 1));
+
+            Assert.AreEqual(5, Division.Calculation(5, 1));
+        }
+    }
+}
