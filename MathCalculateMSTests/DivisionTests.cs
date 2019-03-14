878a9d88e2559bd0a06cd5ad3aaa050a915dfc18
wugenshui <963838470@qq.com>
2019-03-14 10:37:39 +0800

    nunit3

--- MathCalculateMSTests/DivisionTests.cs
+++ MathCalculateMSTests/DivisionTests.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace MathCalculate.MSTests
+namespace MathCalculate.MathCalculateNUnit3Tests
 {
     [TestClass()]
     public class DivisionTests
---
+++ MathCalculateNUnit3Tests/DivisionTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using MathCalculate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MathCalculate.MathCalculateNUnit3Tests
+{
+    [TestFixture()]
+    public class DivisionTests
+    {
+        [Test()]
+        [TestCase(3, 2, ExpectedResult = 1.5)]
+        [TestCase(700, 100, ExpectedResult = 7)]
+        [TestCase(3, 1, ExpectedResult = 3)]
+        [TestCase(3888, 2, ExpectedResult = 1944)]
+        public double CalculationTest(double divisor, double dividend)
+        {
+            return Division.Calculation(divisor, dividend);
+        }
+    }
+}
